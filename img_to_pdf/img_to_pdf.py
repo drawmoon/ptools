b30721ac4085813b629e51473d9916569535e2eb
drsh <41671372+drawmoon@users.noreply.github.com>
2022-08-02 13:47:47 +0800

    image to pdf

--- img_to_pdf/img_to_pdf.py
+++ img_to_pdf/img_to_pdf.py
@@ -6,22 +6,26 @@ from PIL import Image
 
 
 def isimage(file: str) -> bool:
-    img_ext = [".jpg", ".png"]
+    img_ext = [".jpg", ".jpeg", ".png"]
     for ext in img_ext:
-        if file.endswith(ext):
+        if file.lower().endswith(ext):
             return True
     return False
 
 
-filepath = sys.argv[1]
-if os.path.isdir(filepath):
-    img_list = []
-    for filename in os.listdir(filepath):
-        if not isimage(filename):
-            continue
-        img = Image.open(os.path.join(filepath, filename)).convert("RGB")
-        img_list.append(img)
-    img_list.pop(0).save(Path.cwd() / "export.pdf", save_all=True, append_images=img_list)
-else:
-    img = Image.open(filepath).convert("RGB")
-    img.save(Path.cwd() / "export.pdf")
+def topdf(filepath: str, outfile: str):
+    if os.path.isdir(filepath):
+        img_list = []
+        for filename in os.listdir(filepath):
+            if not isimage(filename):
+                continue
+            img = Image.open(os.path.join(filepath, filename)).convert("RGB")
+            img_list.append(img)
+        img_list.pop(0).save(Path.cwd() / outfile, save_all=True, append_images=img_list)
+    else:
+        img = Image.open(filepath).convert("RGB")
+        img.save(Path.cwd() / outfile)
+
+
+if __name__ == "__main__":
+    topdf(sys.argv[1], "export.pdf")
